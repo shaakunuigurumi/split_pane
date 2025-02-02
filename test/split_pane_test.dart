e8d5af30dcc9816d3d0c09b769ab1bdaac73490b
shaakunuigurumi <188075782+shaakunuigurumi@users.noreply.github.com>
2025-02-03 00:52:02 +0100

    Split tests

--- test/split_pane_test.dart
+++ test/split_pane_test.dart
@@ -3,8 +3,28 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:split_pane/split_pane.dart';
 
 void main() {
-  testWidgets('Fraction is correctly laid out', (widgetTester) async {
+  testWidgets('Panes are equal width on 50%', (widgetTester) async {
     final controller = SplitController(vsync: widgetTester);
+
+    final primaryKey = ValueKey(1);
+    final secondaryKey = ValueKey(2);
+
+    await widgetTester.pumpWidget(
+      SplitPane(
+        controller: controller,
+        primary: SizedBox.expand(key: primaryKey),
+        secondary: SizedBox.expand(key: secondaryKey),
+      ),
+    );
+
+    var primarySize = widgetTester.getSize(find.byKey(primaryKey));
+    var secondarySize = widgetTester.getSize(find.byKey(secondaryKey));
+
+    expect(primarySize, secondarySize);
+  });
+
+  testWidgets('Panes are correctly laid out on 25%', (widgetTester) async {
+    final controller = SplitController(vsync: widgetTester, position: .25);
 
     final primaryKey = ValueKey(1);
     final secondaryKey = ValueKey(2);
@@ -21,15 +41,6 @@ void main() {
 
     var primarySize = widgetTester.getSize(find.byKey(primaryKey));
     var secondarySize = widgetTester.getSize(find.byKey(secondaryKey));
-
-    expect(primarySize, secondarySize);
-
-    controller.setToFraction(0.25);
-
-    await widgetTester.pump();
-
-    primarySize = widgetTester.getSize(find.byKey(primaryKey));
-    secondarySize = widgetTester.getSize(find.byKey(secondaryKey));
 
     final remainingSize =
         splitPaneSize.width - primarySize.width - secondarySize.width;
